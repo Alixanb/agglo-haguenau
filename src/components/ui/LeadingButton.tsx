1cc42798a7d398433725cd1dd043c5a385a9145e
Alixan <alixan.balu@etu.unistra.fr>
2024-05-07 09:49:25 +0200

    remade leading button component to take lucide react icons

--- src/components/ui/LeadingButton.tsx
+++ src/components/ui/LeadingButton.tsx
@@ -4,44 +4,41 @@ import React from "react";
 import { P } from "../typos";
 
 // Buton classes
-const leadingButtonVariants = cva(
-  "flex gap-4 rounded-md items-center text-slate-600",
-  {
-    variants: {
-      variant: {
-        default: "border border-slate-200 p-1 pr-4",
-        dark: "border border-slate-600 p-1 pr-4",
-        buttonOnly: "",
-        active: "bg-blue-100 p-1 pr-4",
-      },
-      size: {
-        default: "w-fit",
-        full: "w-full-3",
-      },
+const leadingButtonVariants = cva("flex gap-4 rounded-md items-center curre", {
+  variants: {
+    variant: {
+      default: "border border-slate-200 p-1 pr-4",
+      dark: "border border-slate-600 p-1 pr-4",
+      buttonOnly: "",
+      active: "bg-blue-100 p-1 pr-4",
     },
-    defaultVariants: {
-      variant: "default",
-      size: "default",
+    size: {
+      default: "w-fit",
+      full: "w-full-3",
     },
-  }
-);
+  },
+  defaultVariants: {
+    variant: "default",
+    size: "default",
+  },
+});
 
 //Icon classes
 const leadingButtonIconVariants = cva("rounded-sm p-2", {
   variants: {
     button: {
-      slate: "bg-slate-100 fill-slate-600",
-      dark: "bg-slate-600 fill-slate-100",
-      gray: "bg-gray-100 fill-gray-600",
-      black: "bg-black fill-gray-400",
-      blue: "bg-blue-100 fill-blue-600",
-      red: "bg-red-100 fill-red-600",
-      sky: "bg-sky-100 fill-sky-600",
-      yellow: "bg-yellow-100 fill-yellow-600",
-      orange: "bg-orange-100 fill-orange-600",
-      green: "bg-green-100 fill-green-600",
-      lime: "bg-lime-100 fill-lime-600",
-      purple: "bg-purple-100 fill-purple-600",
+      slate: "bg-slate-100 stroke-slate-600",
+      dark: "bg-slate-600 stroke-slate-100",
+      gray: "bg-gray-100 stroke-gray-600",
+      black: "bg-black stroke-gray-400",
+      blue: "bg-blue-100 stroke-blue-600",
+      red: "bg-red-100 stroke-red-600",
+      sky: "bg-sky-100 stroke-sky-600",
+      yellow: "bg-yellow-100 stroke-yellow-600",
+      orange: "bg-orange-100 stroke-orange-600",
+      green: "bg-green-100 stroke-green-600",
+      lime: "bg-lime-100 stroke-lime-600",
+      purple: "bg-purple-100 stroke-purple-600",
     },
   },
   defaultVariants: {
@@ -74,10 +71,18 @@ interface LeadingButtonProps
 }
 
 /**
- * A button with a colored and styled icon
+ * LeadingButton Component
  *
- * @param param0 Takes classical HTML tags
- * @todo Make this turnable into a Link Component
+ * Renders a button with a colored and styled icon.
+ *
+ * @param className Additional CSS classes for the button element.
+ * @param variant Variant of the button (e.g., default, dark, active).
+ * @param size Size of the button (e.g., default, full).
+ * @param button Button variant (e.g., slate, dark, gray).
+ * @param children Child elements to be rendered inside the button. Must be a LucideReact SVG And a text
+ * @param link Type of link associated with the button (e.g., default, out, next).
+ * @param src Source URL for the button.
+ * @param props Additional HTML attributes to be passed to the button element.
  * @returns A button with a styled icon.
  */
 const LeadingButton: React.FC<LeadingButtonProps> = ({
@@ -98,14 +103,18 @@ const LeadingButton: React.FC<LeadingButtonProps> = ({
 
   const icon = React.Children.toArray(children).find((child) =>
     testButton(child)
-  );
+  ) as React.ReactElement<any, string>;
 
   return (
     <button
       className={cn(leadingButtonVariants({ variant, size, className }))}
       {...props}
     >
-      <span className={cn(leadingButtonIconVariants({ button }))}>{icon}</span>
+      <span className={cn(leadingButtonIconVariants({ button }))}>
+        {React.cloneElement(icon, {
+          stroke: "default",
+        })}
+      </span>
       <div className="w-full justify-between">
         {text && <P variant="medium">{text}</P>}
         {link === "out" && true}
