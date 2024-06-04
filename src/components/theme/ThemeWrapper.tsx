3a31de77b0d2fb1f276647411a2edaf27bfb7cc9
Alixan <alixan.balu@etu.unistra.fr>
2024-06-04 09:27:20 +0200

    chore: removed capacitor for build

--- src/components/theme/ThemeWrapper.tsx
+++ src/components/theme/ThemeWrapper.tsx
@@ -1,6 +1,5 @@
-import type { Metadata } from "next";
 import "@css";
-import { Preferences } from "@capacitor/preferences";
+import type { Metadata } from "next";
 
 export const metadata: Metadata = {
   title: "Agglomération de Haguenau",
@@ -12,16 +11,6 @@ const ThemeWrapper = ({
 }: Readonly<{
   children: React.ReactNode;
 }>) => {
-  const getTheme = async () => {
-    const ret = await Preferences.get({ key: "theme" });
-    const theme = JSON.parse(ret.value!);
-    console.log("t ouuu", theme);
-  };
-
-  if (typeof window !== `undefined` && "Proxy" in window) {
-    getTheme();
-  }
-
   return <div>{children}</div>;
 };
 
